2efdae9ef88747f110479b586b6884015cfe90eb
Seikened <fernanlee2131@gmail.com>
2023-08-24 08:23:55 -0600

    ASD

--- Proyecto_Python/control-flujo/03-if-ternario.py
+++ Proyecto_Python/control-flujo/03-if-ternario.py
@@ -1,1 +1,10 @@
-Test PRUEBA
+edad = 12
+
+mensaje = "Es mayor de edad👴🏼" if edad > 17 else "Es menor de edad👦🏼"
+
+# if edad > 17:
+# 	mensaje = "Es mayor de edad"
+# else:
+# 	mensaje = "Es menor de edad"
+
+print(mensaje)
---
+++ Proyecto_Python/control-flujo/04-operadores.logicos.py
@@ -0,0 +1,22 @@
+# and or not
+
+# Auto
+
+gas = True
+encendido = True
+edad = 23
+licencia = True
+
+# if not (edad >= 18):
+# 	mensaje = "No puedes conducir, no eres mayor de edad"
+# elif not gas or not encendido:
+# 	mensaje = "El auto no puede ser usado, verifica que tenga gasolina y este encendido"
+# else:
+# 	mensaje = "A MANEJAR!!!" if licencia else "No puedes conducir, no tienes licencia"
+#
+# print(mensaje)
+
+
+if not gas and encendido and edad > 17:
+	mensaje="A MANEJAR!!!"
+
