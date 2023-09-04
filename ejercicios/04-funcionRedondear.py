26ca8792782545d176e0953880cc8de50a95eb14
Seikened <fernanlee2131@gmail.com>
2023-09-04 17:33:19 -0600

    WORK IN PROGRESS🚧...

---
+++ ejercicios/04-funcionRedondear.py
@@ -0,0 +1,8 @@
+numeroDecimal = float(input("Ingrese un numero decimal: "))
+# Dos números decimales
+reduccionDeciaml = (numeroDecimal*10)
+reduccionDeciaml = int(reduccionDeciaml)
+reduccionDeciaml = reduccionDeciaml/10
+
+print(reduccionDeciaml)
+# Reduccion a 1 decimal
