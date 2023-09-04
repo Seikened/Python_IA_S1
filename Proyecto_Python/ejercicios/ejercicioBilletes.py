c10ccbcccceed64d87f479ea55414a2799e4552e
Seikened <fernanlee2131@gmail.com>
2023-09-04 00:27:46 -0600

    versión 3.0 optimización de código

--- Proyecto_Python/ejercicios/ejercicioBilletes.py
+++ Proyecto_Python/ejercicios/ejercicioBilletes.py
@@ -1,59 +1,52 @@
-# def billetes(monto,valor_billete):
-# 	cantidad_billetes = 0
-# 	while (monto // valor_billete) != 0:
-# 		cantidad_billetes += 1
-# 		monto -= valor_billete
-# 	return monto, cantidad_billetes
+# def billetes(monto, valor_billete):
+#     cantidad_billetes = monto // valor_billete
+#     monto %= valor_billete
+#     return monto, cantidad_billetes
 #
 # monto = int(input("Ingrese el monto: "))
 # monto_copia = monto
-# monto, billete_1000 = billetes(monto,1000)
-# monto, billete_500 = billetes(monto,500)
-# monto, billete_200 = billetes(monto,200)
-# monto, billete_100 = billetes(monto,100)
-# monto, billeteQuinientos = billetes(monto, 50)
 #
+# monto, billete_1000 = billetes(monto, 1000)
+# monto, billete_500 = billetes(monto, 500)
+# monto, billete_200 = billetes(monto, 200)
+# monto, billete_100 = billetes(monto, 100)
+# monto, billete_50 = billetes(monto, 50)
 #
 # if monto == 0:
-# 	print("Se puede dar el monto exacto")
-# 	print("Billetes de 1000: ", billete_1000)
-# 	print("Billetes de 500: ", billete_500)
-# 	print("Billetes de 200: ", billete_200)
-# 	print("Billetes de 100: ", billete_100)
-# 	print("Billetes de 50: ", billeteQuinientos)
-# 	print("Monto: ", monto_copia)
+#     print("Se puede dar el monto exacto")
+#     print("Billetes de 1000:", billete_1000)
+#     print("Billetes de 500:", billete_500)
+#     print("Billetes de 200:", billete_200)
+#     print("Billetes de 100:", billete_100)
+#     print("Billetes de 50:", billete_50)
+#     print("Monto:", monto_copia)
 # else:
-# 	print("No se puede dar el monto exacto")
+#     print("No se puede dar el monto exacto")
 
 
 
-monto = 2250
 
+# Programa de cajero automático para entregar la menor cantidad de billetes
+
+
+RetiroTotal = 1850  # Total a retirar
 
 # Billetes de 500
-cantidad_billetes = 0 # Crear una variable en cero
-operación = monto // 500 # Verifico que mi monto se pueda dividir exacatamente entre los 500 pesos
-cantidad_billetes += operación # Aquí lo que hago es sumar la cantidad de billetes a mi variable cantidad_billetes
-monto -= 500 * operación
-print("Billetes de 500: ", cantidad_billetes)
+B500 = RetiroTotal // 500
+RetiroTotal %= 500
+print("Billetes de 500: ", B500)
 
-# Billetes de 250
-cantidad_billetes = 0
-operación = monto // 250
-cantidad_billetes += operación
-monto -= 250 * operación
-print("Billetes de 250: ", cantidad_billetes)
+# Billetes de 200
+B200 = RetiroTotal // 200
+RetiroTotal %= 200
+print("Billetes de 200: ", B200)
 
 # Billetes de 100
-cantidad_billetes = 0
-operación = monto // 100
-cantidad_billetes += operación
-monto -= 100 * operación
-print("Billetes de 100: ", cantidad_billetes)
+B100 = RetiroTotal // 100
+RetiroTotal %= 100
+print("Billetes de 100: ", B100)
 
 # Billetes de 50
-cantidad_billetes = 0
-operación = monto // 50
-cantidad_billetes += operación
-monto -= 50 * operación
-print("Billetes de 50: ", cantidad_billetes)
+B50 = RetiroTotal // 50
+RetiroTotal %= 50
+print("Billetes de 50: ", B50)
--- Proyecto_Python/ejercicios/formulaGeneral.py
+++ Proyecto_Python/ejercicios/formulaGeneral.py
@@ -7,7 +7,7 @@ c = 3
 raiz = (mt.sqrt((b ** 2) - 4 * (a * c)))
 
 xPositiva = ((-b) + raiz)  / (2 * a)
-xNegativa = ((-b) - raiz) / (2*a)
+xNegativa = ((-b) - raiz) / (2 * a)
 
 print(f"""
 x en positivo es: {xPositiva}
