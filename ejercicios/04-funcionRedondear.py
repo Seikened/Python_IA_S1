b92321e5d3d0d4825a6af03dab57ca2dfd33c906
Seikened <fernanlee2131@gmail.com>
2023-09-04 22:17:47 -0600

    WORK IN PROGRESS🚧...

--- ejercicios/04-funcionRedondear.py
+++ ejercicios/04-funcionRedondear.py
@@ -1,8 +1,29 @@
-numeroDecimal = float(input("Ingrese un numero decimal: "))
-# Dos números decimales
-reduccionDeciaml = (numeroDecimal * 10)
-reduccionDeciaml = int(reduccionDeciaml)
-reduccionDeciaml = reduccionDeciaml / 10
+numeroDecimal =  9.86567
+int(input("Ingrese el numero de decimales que desea: "))
+factor = 10 ** n_decimales
 
-print(reduccionDeciaml)
-# Reduccion a 1 decimal
+
+# Un números decimales
+numeroUnDigitos = numeroDecimal
+reduccionDecimalUnDigito = (numeroUnDigitos + 0.05)
+reduccionDecimalUnDigito *=  10
+reduccionDecimalUnDigito = int(reduccionDecimalUnDigito)
+reduccionDecimalUnDigito /=  10
+
+print("El numero decimal ingresado es: ", reduccionDecimalUnDigito)
+# Dos números decimales
+numeroDosDigitos = numeroDecimal
+reduccionDecimalDosDigitos = (numeroDosDigitos + 0.005)
+reduccionDecimalDosDigitos *=  100
+reduccionDecimalDosDigitos = int(reduccionDecimalDosDigitos)
+reduccionDecimalDosDigitos /=  100
+
+print("El numero decimal ingresado es: ", reduccionDecimalDosDigitos)
+# Tres números decimales
+numeroTresDigitos = numeroDecimal
+reduccionDecimalTresDigitos = (numeroTresDigitos + 0.0005)
+reduccionDecimalTresDigitos *=  1000
+reduccionDecimalTresDigitos = int(reduccionDecimalTresDigitos)
+reduccionDecimalTresDigitos /=  1000
+
+print("El numero decimal ingresado es: ", reduccionDecimalTresDigitos)
