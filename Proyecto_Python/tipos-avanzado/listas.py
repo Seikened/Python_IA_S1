10aef5b971f58f75c6502414362d11f23fbaeb04
Seikened <fernanlee2131@gmail.com>
2023-09-14 08:50:22 -0600

    Work in Progress 🚧...

---
+++ Proyecto_Python/tipos-avanzado/listas.py
@@ -0,0 +1,12 @@
+numeros = [1,2,3]
+letras = ["a","b","c"]
+palabras = ["Chancho","Feliz","Lunes"]
+palabrasFelices = ["Feliz","Contento","Alegre"]
+booleans = [True,False,True]
+matriz = [[1,2],[3,4]]
+ceros = [0] * 10
+alfanumerico = numeros+letras
+rango = list(range(1, 11))
+char = list("Hola Mundo")
+
+print(char)
