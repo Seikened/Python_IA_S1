fa8a8d047c896878e49bf726baaf07f1dd84a90d
Seikened <fernanlee2131@gmail.com>
2023-09-18 09:59:16 -0600

    Working in progress🚧...

--- ejerciciosProgramacion/A14_tornilloCoordenadas.py
+++ ejerciciosProgramacion/A14_tornilloCoordenadas.py
@@ -1,20 +1,10 @@
-# Función para calcular x y y en función de c
-def calcular_xy(c_x_mm, c_y_mm):
-	x = (c_x_mm - 2.5) // 15
-	y = (c_y_mm - 2.5) // 15
-	# Despeje de n
-	n = (y * 8) + x
-	return n
+c_x_mm = float(input("Ingrese la coordenada de X: "))
+c_y_mm = float(input("Ingrese la coordenada de Y: "))
+columna = (c_x_mm - 2.5) // 15
+renglon = (c_y_mm - 2.5) // 15
+# Despeje de n
+n = (renglon * 8) + columna
+resultado = int(n)
 
+print(f"""Para la coordenana de columna: 🔹{c_x_mm}🔹 y las de renglon: 🔹{c_y_mm}🔹El tornillo buiscado es: {resultado}""")
 
-if __name__ == "__main__":
-	# Coordenadas introducidas por el usuario para x y y
-	c_x_mm = float(input("Ingrese la coordenada de X: "))
-	c_y_mm = float(input("Ingrese la coordenada de Y: "))
-
-	# Calculamos x y y
-	resultado = int(calcular_xy(c_x_mm, c_y_mm))
-
-	print(f"""Para la coordenana de X: 🔹{c_x_mm}🔹 y las de Y: 🔹{c_y_mm}🔹
-El tornillo buiscado es: {resultado}
-""")
