057d3ca5bc9c32aa4f72825f8941d16aa0f5be80
Seikened <fernanlee2131@gmail.com>
2023-09-20 23:28:54 -0600

    Working in progress🚧...

--- fomulasProbabilidad/A3_distribucionPoisson.py
+++ fomulasProbabilidad/A3_distribucionPoisson.py
@@ -1,18 +1,17 @@
+import math  # Para la constante de Euler
+
 def factorial(numero):
-	factorial = 1
-	for i in range(1, numero + 1):
-		factorial = factorial * i
-	return factorial
+    factorial = 1
+    for i in range(1, numero + 1):
+        factorial *= i
+    return factorial
 
-
-def distribucionPoisson(lambdaVariable, numeroOcurrencias):
-	euler = 2.7182818284590452353602874713527
-	distribucionPoisson = ((lambdaVariable ** numeroOcurrencias) * (euler ** (-lambdaVariable))) / factorial(
-		numeroOcurrencias)
-	return distribucionPoisson
-
+def distribucionPoisson(lambda_var, numero_ocurrencias):
+    euler = math.e  # Usamos la constante de Euler del módulo math
+    resultado = ((lambda_var ** numero_ocurrencias) * (math.exp(-lambda_var))) / factorial(numero_ocurrencias)
+    return resultado
 
 if __name__ == '__main__':
-	print(f"La distribucion de poisson es: {distribucionPoisson(lambdaUser, numeroOcurrenciasUser)}")
-	lambdaUser = float(input("Ingrese el valor de lambda: "))
-	numeroOcurrenciasUser = int(input("Ingrese el valor de numero de ocurrencias: "))
+    lambda_var = float(input("Ingrese el valor de lambda: "))
+    numero_ocurrencias = int(input("Ingrese el valor de número de ocurrencias: "))
+    print(f"La distribución de Poisson es: {distribucionPoisson(lambda_var, numero_ocurrencias)}")
