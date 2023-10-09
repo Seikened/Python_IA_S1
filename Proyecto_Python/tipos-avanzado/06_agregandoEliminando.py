1a1d46cb506219dfd69b6dc8f4ae2321294901e7
Seikened <fernanlee2131@gmail.com>
2023-10-09 10:55:44 -0600

    Work in progress 🚧...

--- Proyecto_Python/tipos-avanzado/06_agregandoEliminando.py
+++ Proyecto_Python/tipos-avanzado/06_agregandoEliminando.py
@@ -11,7 +11,8 @@ mascotas.insert(1, 'Melvin')  # Agrega un elemento en la posición indicada
 mascotas.append('Panda')  # Agrega un elemento al final de la lista
 mascotas.remove('Felipe')  # Elimina el primer elemento que coincida con el valor indicado
 mascotas.pop()  # Elimina el último elemento de la lista
-mascotas.pop(1)  # Elimina el elemento de la posición indicada
+eliminado = mascotas.pop(1)  # Elimina el elemento de la posición indicada
+print("Eliminaste:",eliminado)
 del mascotas[0]  # Elimina el elemento de la posición indicada
-mascotas.clear()  # Elimina todos los elementos de la lista
+#mascotas.clear()  # Elimina todos los elementos de la lista
 print(mascotas)
