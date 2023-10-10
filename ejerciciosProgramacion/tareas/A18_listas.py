0cec2025385b8c6273f6599b195c2a18651021f6
Seikened <fernanlee2131@gmail.com>
2023-10-10 17:36:24 -0600

    Work in progress 🚧...

---
+++ ejerciciosProgramacion/tareas/A18_listas.py
@@ -0,0 +1,31 @@
+listaNumerosFlotantes = []
+cantidadNumerosLista = int(input("¿De cuantos números quieres tu lista? "))
+numeroMayor = float('-inf')
+numeroMenor = float('inf')
+sumaTotal = 0
+promedio = 0
+
+for _ in range(cantidadNumerosLista):
+    numeroFlotante = float(input("Introduce tu número: "))
+    listaNumerosFlotantes.append(numeroFlotante)
+    
+print(f"Tu lista es: {listaNumerosFlotantes}")
+
+
+for numero in listaNumerosFlotantes:
+    if numero > numeroMayor:
+        numeroMayor = numero
+
+    if numero < numeroMenor:
+        numeroMenor = numero
+print(f"""
+El menor de tus números es: {numeroMenor}
+El mayor de tus núemros es: {numeroMayor}
+""")
+
+for numero in listaNumerosFlotantes:
+    sumaTotal += numero 
+    
+promedio = sumaTotal/ len(listaNumerosFlotantes)
+
+print(f"El promedio de tu lista {listaNumerosFlotantes} es: {promedio}")
