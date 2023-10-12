09c5c63d00a93b316ff041451dc3d6e07d36750c
Seikened <fernanlee2131@gmail.com>
2023-10-12 13:15:12 -0600

    Work in progress 🚧...

--- Proyecto_Python/A03_matrices.py
+++ Proyecto_Python/A03_matrices.py
@@ -4,20 +4,24 @@ def numeroMatriz():
      numero = random.randint(-10,10)
      return numero
 
-columnasA = 3 #int(input("Cuantas colunmas de la matríz a: "))
-renglonesA = 3 #int(input("Cuantos renglones de la matríz a: "))
-
+while True:
+     # Matríz a
+     columnasA = 3 #int(input("Cuantas colunmas de la matríz a: "))
+     renglonesA = 3 #int(input("Cuantos renglones de la matríz a: "))
+     # Matríz b
+     columnasB = 3 #int(input("Cuantas colunmas de la matríz b: "))
+     renglonesB = 3 #int(input("Cuantos renglones de la matríz b: "))
+     if columnasA == renglonesB:
+          break
 
 a = [[numeroMatriz() for _ in range(columnasA)] for _ in range(renglonesA)]
 print(f"MATRÍZ A: \n{a}\n")
 
-
-columnasB = 3 #int(input("Cuantas colunmas de la matríz b: "))
-renglonesB = 3 #int(input("Cuantos renglones de la matríz b: "))
-
 b = [[numeroMatriz() for _ in range(columnasB)] for _ in range(renglonesB)]
 print(f"MATRÍZ B: \n{b}\n")
+     
 
+lista = [[] ]
 
 # Tamaño de la matríz c
 c = [[0 for _ in range(len(b[0]))] for _ in range(len(a))]
