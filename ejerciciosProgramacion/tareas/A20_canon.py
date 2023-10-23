32cd483874e04ad16f10fc6f9c41fe126ba195af
Seikened <fernanlee2131@gmail.com>
2023-10-23 08:37:06 -0600

    Work in progress 🚧...

--- ejerciciosProgramacion/tareas/A20_canon.py
+++ ejerciciosProgramacion/tareas/A20_canon.py
@@ -2,6 +2,8 @@ import math as mt
 import matplotlib.pyplot as plt
 
 
+
+
 pi = mt.pi
 velocidadInicial_m_s = 400
 aceleracion_m_s2 = -9.81
@@ -11,7 +13,8 @@ anguloVector = [0 for _ in range(19)]
 
 print("\n  Angulo   |  Distancia del cañon")
 print("-----------|--------------")
-
+gradoDerecho = 0
+listaGradoDerecho = []
 for i,angulo in enumerate(range(0,91,5)):
     radianes = (pi * angulo) / 180
     distanicaMisil = ((-2 * (velocidadInicial_m_s**2) ) * (mt.cos(radianes) * mt.sin(radianes))) / aceleracion_m_s2
@@ -21,13 +24,14 @@ for i,angulo in enumerate(range(0,91,5)):
     
 print("\n")
 
+
 # Verificar que exista el mayor y los disparos angulos son resultados similares
 distanciaMayor = 0
 anguloMayor = 0 
 
 # Verificación de angulos iguales dentro del vector dando que dos angulos pueden dar la misma distancia
 numero_de_vecesDistancia = []
-
+nueva_entrada = {}
 for i, (distancia, angulo) in enumerate(zip(distanicaMisilVector, anguloVector)):
     
     if distanciaMayor < distancia:
@@ -49,11 +53,8 @@ for i, (distancia, angulo) in enumerate(zip(distanicaMisilVector, anguloVector))
         # Si no existe, añadimos la nueva entrada al diccionario
         numero_de_vecesDistancia.append(nueva_entrada)
 
-    
+print(numero_de_vecesDistancia)
 print(f"El mejor angulo del cañon desde: {anguloVector[0]} hasta: {anguloVector[-1]} es el angulo {anguloMayor}")
-
-
-
 
 
 plt.plot(anguloVector,distanicaMisilVector, color="#11d1dd", linestyle="--", marker="o", label=" Distancia del cañon") 
---
+++ ejerciciosProgramacion/tareas/diccionario.py
@@ -0,0 +1,18 @@
+diccionario = [
+            {'posicion': 0, 'angulo': 0, 'distancia': 0.0}, 
+            {'posicion': 1, 'angulo': 5, 'distancia': 2832.1823064942764}, 
+            {'posicion': 2, 'angulo': 10, 'distancia': 5578.310186759121}, 
+            {'posicion': 3, 'angulo': 15, 'distancia': 8154.943934760447}, 
+            {'posicion': 4, 'angulo': 20, 'distancia': 10483.793837904821}, 
+            {'posicion': 5, 'angulo': 25, 'distancia': 12494.09897034011}, 
+            {'posicion': 6, 'angulo': 30, 'distancia': 14124.777227880752}, 
+            {'posicion': 7, 'angulo': 35, 'distancia': 15326.281276834387}, 
+            {'posicion': 8, 'angulo': 40, 'distancia': 16062.104024663939}, 
+            {'posicion': 9, 'angulo': 45, 'distancia': 16309.887869520897}, 
+            {'posicion': 10, 'angulo': 50, 'distancia': 16062.104024663946}, 
+            {'posicion': 12, 'angulo': 60, 'distancia': 14124.777227880755}, 
+            {'posicion': 14, 'angulo': 70, 'distancia': 10483.793837904823}, 
+            {'posicion': 16, 'angulo': 80, 'distancia': 5578.310186759124}, 
+            {'posicion': 17, 'angulo': 85, 'distancia': 2832.1823064942755}, 
+            {'posicion': 18, 'angulo': 90, 'distancia': 1.997385197386101e-12}
+            ]
