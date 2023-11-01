856a1cde8875c4009d50c49c2a99152968aa86d8
Seikened <fernanlee2131@gmail.com>
2023-11-01 17:15:54 -0600

    Update parcial inputs and variable names in A06_listaCalificaciones.py

--- ejerciciosProgramacion/repasoExamen/A06_listaCalificaciones.py
+++ ejerciciosProgramacion/repasoExamen/A06_listaCalificaciones.py
@@ -4,12 +4,13 @@
 grupos= []
 for i in range (0,3) :
     nombre=input ("Nombre?")
-    parcial_1=float (input ("Parciall?"))
-    parcial_2=float (input ("Parcia12?") )
-    parcial_3=float (input ("Parcia13?"))
+    parcial_1=float (input ("Primer parcial: "))
+    parcial_2=float (input ("Segundo parcial: ") )
+    parcial_3=float (input ("Tercer parcial: "))
     parciales=[parcial_1, parcial_2,parcial_3]
-    alumno1= [nombre, parciales]
-    grupos.append (alumno1)
+    alumno= [nombre, parciales]
+    grupos.append (alumno)
     
 #Aqui mostrarlos
 print(grupos)
+
