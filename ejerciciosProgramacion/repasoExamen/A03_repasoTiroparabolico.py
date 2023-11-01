a2b86a02e44ff7620918821906681ec22e1a7c31
Seikened <fernanlee2131@gmail.com>
2023-11-01 16:50:41 -0600

    Remove unnecessary code for object height calculation

--- ejerciciosProgramacion/repasoExamen/A03_repasoTiroparabolico.py
+++ ejerciciosProgramacion/repasoExamen/A03_repasoTiroparabolico.py
@@ -30,16 +30,3 @@ plt.ylabel("Altura [m]")
 plt.grid(True)
 plt.legend()
 plt.show()
-
-
-# Tercera parte
-tiempo_s = 0
-alturaObjeto = 0
-
-while  alturaObjeto >= 1.8:
-    alturaObjeto = alturaOriginal_m + ( velocidadOriginal_m_s * tiempo_s ) + ( .5 * ( aceleracionGravedad_m_s2 * ( tiempo_s ** 2 ) ) )
-    print("  %6.2f  |   %6.2f" %(tiempo_s, alturaObjeto))
-    tiempo_s += .001
-    alturaObjeto += 0.001
-
-print(f"termine, el tiempo del objeto es: {tiempo_s}")
