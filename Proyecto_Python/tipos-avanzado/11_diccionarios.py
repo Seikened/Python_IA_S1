eed45827fec5d45ded43ff0d1903b578815f6047
Seikened <fernanlee2131@gmail.com>
2023-11-16 10:47:27 -0600

    Fix dictionary syntax and add examples

--- Proyecto_Python/tipos-avanzado/11_diccionarios.py
+++ Proyecto_Python/tipos-avanzado/11_diccionarios.py
@@ -1,6 +1,6 @@
 punto = {
 	"x": 25,
-	"y": 50
+	"y": 50,
 }
 
 print(punto["x"])
@@ -12,3 +12,34 @@ if "lala" in punto:
 	print("Encontre lala", punto["lala"])
 else:
 	print("No encontre lala")
+
+print(punto.get("x"))
+print(punto.get("w",97))
+
+del punto["x"]
+del(punto["y"])
+
+print(punto)
+
+punto["x"] = 50
+
+for valor in punto:
+    print(valor,punto[valor])
+
+
+for valor in punto.items():
+    print(valor)
+
+for llave, valor in punto.items():
+    print(valor, llave)
+
+
+usuarios = [
+	{"id":1, "nombre":"chanchito"},
+	{"id":2, "nombre":"Fernando"},
+	{"id":3, "nombre":"Leon"}
+]
+
+for usuario in usuarios:
+    print(usuario["nombre"])
+
